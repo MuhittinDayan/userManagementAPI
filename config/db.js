f1f48a2db97a3bf198b8c14358486c96df663cd9
MuhittinDayan <mhtndyn.34@gmail.com>
2026-07-14 17:57:31 +0300

    feat: configure MySQL connection pool

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2/promise") ;
+require("dotenv").config();
+
+
+const db = mysql.createPool({
+    "host" : "localhost" ,
+    "user" : "root" ,
+    "password" : process.env.DB_PASSWORD,
+    "database" : process.env.DB_DATABASE
+}) ;
+
+module.exports = db ;
